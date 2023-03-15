f986a59570aae516f2bebbef82487a8c90599c01
busrahadimlioglu <busrahadimlioglu@gmail.com>
2023-03-15 21:07:02 +0300

    routes and utils create

---
+++ lib/routes/previewpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Previewpage extends StatefulWidget {
+  const Previewpage({Key? key}) : super(key: key);
+
+  @override
+  _PreviewpageState createState() => _PreviewpageState();
+}
+
+class _PreviewpageState extends State<Previewpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
